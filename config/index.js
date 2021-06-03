985d250fb3b8b9535d19ed59b24b8e9a7c124036
zhangyalong <zhangyalong@meituan.com>
2021-06-03 19:17:06 +0800

    feature/add_visitor fix index.js

--- config/index.js
+++ config/index.js
@@ -3,4 +3,5 @@ module.exports = {
   password: process.env.TEXTNOW_PASSWORD || "",
   recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
+  urls: process.env.AUTL_VISIT_URLS || "https://test-1c998b-1251225310.ap-shanghai.app.tcloudbase.com/thread/2",
 };
